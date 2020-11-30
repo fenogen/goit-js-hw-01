5d80db4e403417726bcf300f65ea4fc0cf7996c1
Oleg Soldatenko <sunskays@gmail.com>
2020-11-30 18:58:22 +0200

    Подготовка

---
+++ js/task_1.js
@@ -0,0 +1,15 @@
+let a = 23;
+
+console.log(a);
+
+
+const ukraine = confirm('kiev');
+
+console.log(ukraine);
+
+const data = prompt('vvedite chislo');
+
+console.log(data);
+
+console.log(5 + 5 + '5');
+
